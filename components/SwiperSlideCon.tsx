175fe7f9dad28b1ee19dbd4ff98873c561850b04
chae-young <cycy8527@gmail.com>
2021-12-29 01:14:41 +0900

    New Upload

--- components/SwiperSlideCon.tsx
+++ components/SwiperSlideCon.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import SwiperCore, { Navigation } from "swiper";
+// import "swiper/css";
+// import "swiper/css/navigation";
 
-const SwiperSlideCon = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+// SwiperCore.use([Navigation]);
 
-export default SwiperSlideCon;
+// interface SwiperSlideProps {
+//     children : React.ReactNode,
+
+// }
+
+// const Responsive = {
+//     "640": {
+//         slidesPerView: 2,
+//         spaceBetween: 20,
+//     },
+//     "768": {
+//         slidesPerView: 3,
+//         spaceBetween: 40,
+//     },
+//     "1024": {
+//         slidesPerView: 5,
+//         spaceBetween: 20,
+//     },
+// };
+
+// const SwiperSlideCon = ({
+//     children,
+// }: SwiperSlideProps) => {
+//     return (
+
+//         <SwiperSlide className=""><>
+//         {children}
+//     )
+// };
+
+// export default SwiperSlideCon;
